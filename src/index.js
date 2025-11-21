d617d8e81a5c93ccdd829f431991d0b35f801f35
Zaid Tariq <zaidtariqshah>
2025-11-21 21:12:37 +0530

    Util code discussion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import connectDB from "./db/index.js";
 import env from "dotenv";
-
+import { app } from "./app.js";
 
 env.config({ path: "./.env" }); // Load environment variables from .env file to process.env
-connectDB();
+
+const startServer = async () => {
+   try {
+      await connectDB();
+      app.listen(process.env.PORT, () => {
+         console.log(`Server is running on port ${process.env.PORT}`);
+      });
+   } catch (error) {
+      console.error("Failed to connect to the database:", errpr);
+   }
+};
+startServer();

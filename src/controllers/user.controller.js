930f3eacea238d42cf5820e943f57784ed06ce3f
Zaid Tariq <zaidtariqshah>
2025-12-12 18:15:51 +0530

    removed unused username

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -234,7 +234,7 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
-   const { fullName, email, username } = req.body;
+   const { fullName, email } = req.body;
    if (!fullName || !email) {
       throw new ApiError(400, "All fields are required");
    }
